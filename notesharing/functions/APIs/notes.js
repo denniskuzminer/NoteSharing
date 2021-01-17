d19e5673c3abc5b56f95a5cf177d78c96e000326
Dennis Kuzminer <denniskuzminer@gmail.com>
2021-01-17 16:34:26 -0500

    Added simple put and delete

--- notesharing/functions/APIs/notes.js
+++ notesharing/functions/APIs/notes.js
@@ -25,42 +25,58 @@ exports.getAllNotes = (request, response) => {
     });
 };
 
-exports.postOneNote = (request, response) => {
-  if (request.body.class.trim() === "") {
-    return response.status(400).json({ class: "Must not be empty" });
-  }
-
-  if (request.body.title.trim() === "") {
-    return response.status(400).json({ title: "Must not be empty" });
-  }
-
-  if (request.body.school.trim() === "") {
-    return response.status(400).json({ school: "Must not be empty" });
-  }
-
-  if (request.body.description.trim() === "") {
-    return response.status(400).json({ description: "Must not be empty" });
-  }
-
-  const newNoteItem = {
-    school: request.body.school,
-    class: request.body.class,
-    title: request.body.title,
-    description: request.body.description,
-    imageURL: request.body.imageURL,
-    createdAt: new Date().toISOString(),
-  };
-  db.collection("notes")
-    .add(newNoteItem)
+/*exports.getOneNote = (request, response) => {
+  db.doc(`/notes/${request.params.noteId}`)
+    .get()
     .then((doc) => {
-      const responseNoteItem = newNoteItem;
-      responseNoteItem.id = doc.id;
-      return response.json(responseNoteItem);
+      if (!doc.exists) {
+        return response.status(404).json({
+          error: "Note not found",
+        });
+      }
+      if (doc.data().username !== request.user.username) {
+        return response.status(403).json({ error: "Unauthorized" });
+      }
+      NoteData = doc.data();
+      NoteData.noteId = doc.id;
+      return response.json(NoteData);
     })
     .catch((err) => {
-      response.status(500).json({ error: "Something went wrong" });
       console.error(err);
+      return response.status(500).json({ error: error.code });
     });
+};*/
+
+exports.postOneNote = (request, response) => {
+  if (request.body.class.trim() === "")
+    return response.status(400).json({ class: "Must not be empty" });
+  else if (request.body.title.trim() === "")
+    return response.status(400).json({ title: "Must not be empty" });
+  else if (request.body.school.trim() === "")
+    return response.status(400).json({ school: "Must not be empty" });
+  else if (request.body.description.trim() === "")
+    return response.status(400).json({ description: "Must not be empty" });
+  else {
+    const newNoteItem = {
+      school: request.body.school,
+      class: request.body.class,
+      title: request.body.title,
+      description: request.body.description,
+      imageURL: request.body.imageURL,
+      createdAt: new Date().toISOString(),
+    };
+    db.collection("notes")
+      .add(newNoteItem)
+      .then((doc) => {
+        const responseNoteItem = newNoteItem;
+        responseNoteItem.id = doc.id;
+        return response.json(responseNoteItem);
+      })
+      .catch((err) => {
+        response.status(500).json({ error: "Something went wrong" });
+        console.error(err);
+      });
+  }
 }; /*{
   "school": "New York University",
   "class": "Computer Systems Organization",
@@ -68,3 +84,41 @@ exports.postOneNote = (request, response) => {
   "description": "Some code stuff",
   "imageURL": ""
 } */
+
+exports.deleteOneNote = (request, response) => {
+  const document = db.doc(`/notes/${request.params.noteId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return response.status(404).json({ error: "Note not found" });
+      }
+      return document.delete();
+    })
+    .then(() => {
+      response.json({ message: "Delete successfull" });
+    })
+    .catch((err) => {
+      console.error(err);
+      return response.status(500).json({ error: err.code });
+    });
+};
+
+exports.editNote = (request, response) => {
+  if (request.body.noteId || request.body.createdAt) {
+    response.status(403).json({ message: "Not allowed to edit" });
+  } else {
+    let document = db.collection("notes").doc(`${request.params.noteId}`);
+    document
+      .update(request.body)
+      .then(() => {
+        response.json({ message: "Updated successfully" });
+      })
+      .catch((err) => {
+        console.error(err);
+        return response.status(500).json({
+          error: err.code,
+        });
+      });
+  }
+};
--- notesharing/functions/index.js
+++ notesharing/functions/index.js
@@ -1,8 +1,17 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 
-const { getAllNotes, postOneNote } = require("./APIs/notes");
+const {
+  getAllNotes,
+  postOneNote,
+  deleteOneNote,
+  editNote,
+  //   getOneNote,
+} = require("./APIs/notes");
 
+app.put("/note/:noteId", editNote);
+// app.get("/note/:noteId", getOneNote);
+app.delete("/note/:noteId", deleteOneNote);
 app.post("/note", postOneNote);
 app.get("/notes", getAllNotes);
 exports.api = functions.https.onRequest(app);

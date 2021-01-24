1e7a60d4175d6dd5ea1934e5a901426b668e874d
Dennis Kuzminer <denniskuzminer@gmail.com>
2021-01-24 00:07:12 -0500

    Add small home

--- notesharing/view/src/App.js
+++ notesharing/view/src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import login from './pages/login';
-import signup from './pages/signup';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import login from "./pages/login";
+import signup from "./pages/signup";
+import home from "./pages/home";
 
-
-import React from 'react';
+import React from "react";
 function App() {
   return (
     <Router>
-        <div>
-          <Switch>
-              <Route exact path="/login" component={login}/>
-              <Route exact path="/signup" component={signup}/>
-          </Switch>
-        </div>
+      <div>
+        <Switch>
+          <Route exact path="/login" component={login} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/" component={home} />
+        </Switch>
+      </div>
     </Router>
   );
 }
